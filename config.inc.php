8b6f5563c1a813339b7814e249c083adf4c6dfe0
北海千川 <64906069+letsite@users.noreply.github.com>
2023-08-09 11:31:08 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -36,7 +36,7 @@ Typecho_Common::init();
 /** 定义数据库参数 */
 $db = new Typecho_Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
-  'host' => 'containers-us-west-176.railway.app',
+  'host' => 'https://containers-us-west-176.railway.app/',
   'user' => 'root',
   'password' => 'BA985BWWIZKJQCdpNpgh',
   'charset' => 'utf8mb4',
